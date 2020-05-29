3e5fbd9e2f1b4f107fdc755e388a648ad57d144b
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-05-29 13:58:37 -0300

    mapeando os elementos de interface utilizando o ViewHolder, adicionando o evendo de click e criando a função par o botão de levar para a DetailsActivity.

--- app/src/main/java/com/gabrieldeespindula/festafimdeano/MainActivity.java
+++ app/src/main/java/com/gabrieldeespindula/festafimdeano/MainActivity.java
@@ -1,13 +1,43 @@
 package com.gabrieldeespindula.festafimdeano;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.textToday = findViewById(R.id.text_today);
+        this.mViewHolder.textDaysLeft = findViewById(R.id.text_days_left);
+        this.mViewHolder.buttonYesNo = findViewById(R.id.button_yes_no);
+
+        this.mViewHolder.buttonYesNo.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button_yes_no){
+            Intent intent = new Intent(getApplicationContext(), DetailsActivity.class);
+            startActivity(intent);
+        }
+    }
+
+
+    private static class ViewHolder {
+        TextView textToday;
+        TextView textDaysLeft;
+        Button buttonYesNo;
     }
 }
