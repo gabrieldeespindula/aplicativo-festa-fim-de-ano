e2a569c27999993b24cec8992cd01d722063249d
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-05-30 13:06:46 -0300

    mapeando check box no DetailsActivity

--- app/src/main/java/com/gabrieldeespindula/festafimdeano/DetailsActivity.java
+++ app/src/main/java/com/gabrieldeespindula/festafimdeano/DetailsActivity.java
@@ -2,12 +2,21 @@ package com.gabrieldeespindula.festafimdeano;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        this.mViewHolder.checkParticipate = findViewById(R.id.check_box_participate);
+    }
+
+    private class ViewHolder {
+        CheckBox checkParticipate;
     }
 }
