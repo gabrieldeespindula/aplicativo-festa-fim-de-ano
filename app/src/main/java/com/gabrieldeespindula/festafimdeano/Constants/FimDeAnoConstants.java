d7b65636e20b8295f0c58bbbf5f0d7ea060c19e4
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-06-01 14:16:48 -0300

    Creating Constants for SharedPreferences; In DetailsActivity, Adding click event in the checkbox and using functions imported from SecurityPreferences.

--- app/src/main/java/com/gabrieldeespindula/festafimdeano/Constants/FimDeAnoConstants.java
+++ app/src/main/java/com/gabrieldeespindula/festafimdeano/Constants/FimDeAnoConstants.java
@@ -1,4 +1,7 @@
 package com.gabrieldeespindula.festafimdeano.Constants;
 
 public class FimDeAnoConstants {
+    public static String PRECENSE_KEY = "presenceKey";
+    public static String CONFIRMATION_YES = "confirmation_yes";
+    public static String CONFIRMATION_NO = "confirmation_no";
 }
--- app/src/main/java/com/gabrieldeespindula/festafimdeano/View/DetailsActivity.java
+++ app/src/main/java/com/gabrieldeespindula/festafimdeano/View/DetailsActivity.java
@@ -2,20 +2,38 @@ package com.gabrieldeespindula.festafimdeano.View;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.CheckBox;
 
+import com.gabrieldeespindula.festafimdeano.Constants.FimDeAnoConstants;
+import com.gabrieldeespindula.festafimdeano.Data.SecurityPreferences;
 import com.gabrieldeespindula.festafimdeano.R;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
+    private SecurityPreferences mSecurityPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
+        this.mSecurityPreferences = new SecurityPreferences(this);
+
         this.mViewHolder.checkParticipate = findViewById(R.id.check_box_participate);
+        this.mViewHolder.checkParticipate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.check_box_participate) {
+            if (this.mViewHolder.checkParticipate.isChecked()) {
+                this.mSecurityPreferences.storeString(FimDeAnoConstants.PRECENSE_KEY, FimDeAnoConstants.CONFIRMATION_YES);
+            }else {
+                this.mSecurityPreferences.storeString(FimDeAnoConstants.PRECENSE_KEY, FimDeAnoConstants.CONFIRMATION_NO);
+            }
+        }
     }
 
     private static class ViewHolder {
